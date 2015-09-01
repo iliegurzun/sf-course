872bce28407ec84d17d08ba5f8c5e07d00b0807f
Ilie Gurzun <ilie.gurzun@emag.ro>
2015-09-01 17:35:14 +0300

    added validation

---
+++ src/AppBundle/Service/NewsletterSender.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ilie.gurzun
+ * Date: 1/9/2015
+ * Time: 4:59 PM
+ */
+
+namespace AppBundle\Service;
+
+use JMS\DiExtraBundle\Annotation\Service;
+use Symfony\Component\Yaml\Parser;
+use JMS\DiExtraBundle\Annotation as DI;
+
+/**
+ * @Service("app.newsletter_sender")
+ *
+ */
+class NewsletterSender
+{
+    /**
+     * @DI\Inject("mailer")
+     * @var \Swift_Mailer
+     */
+    public $mailer;
+
+    public function sendEmail()
+    {
+        $yaml = new Parser();
+        $values = $yaml->parse(file_get_contents(__DIR__ . '/../Resources/config/newsletter.yml'));
+        foreach($values['newsletter']['email_list'] as $value) {
+            if(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                throw new \Exception('Invalid email address');
+            }
+        }
+        $message = \Swift_Message::newInstance()
+            ->setSubject($values['newsletter']['subject'])
+            ->setFrom('ilie.gurzun@emag.ro')
+            ->setTo($values['newsletter']['to'])
+            ->setBody('test',
+                'text/html'
+            );
+        /** @var \Swift_Mailer $resp */
+        $resp = $this->mailer->send($message);
+
+        return $resp;
+    }
+}
